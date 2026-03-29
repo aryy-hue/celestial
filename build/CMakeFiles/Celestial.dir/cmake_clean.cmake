1bbd794d99b4c5ed937b0d1e211b28fe6c7321c5
aryy-hue <wardhana2451@gmail.com>
2026-03-29 22:55:28 +0700

    Initialize openGLl setup with GLFW and GLAD

---
+++ build/CMakeFiles/Celestial.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Celestial.dir/link.d"
+  "CMakeFiles/Celestial.dir/src/glad.c.o"
+  "CMakeFiles/Celestial.dir/src/glad.c.o.d"
+  "CMakeFiles/Celestial.dir/src/main.cpp.o"
+  "CMakeFiles/Celestial.dir/src/main.cpp.o.d"
+  "Celestial"
+  "Celestial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Celestial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Celestial.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Celestial.
+# This may be replaced when dependencies are built.
